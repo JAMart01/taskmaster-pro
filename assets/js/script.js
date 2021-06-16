66f60720d137138828371c3783765d8a93de44c7
JAMart <jorge.martinez1597@gmail.com>
2021-06-16 08:31:19 -0400

    added the ablity to sort tasks into different categories and have the updated categories be saved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var tasks = {};
 
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -60,31 +61,33 @@ $(".list-group").on("click", "p", function() {
   textInput.trigger("focus");
 });
 
+
+
 $(".list-group").on("blur", "textarea", function() {
 
   // get the textarea's current value/text
   var text = $(this)
-  .val()
-  .trim();
+    .val()
+    .trim();
 
   // get the parent's ul's id attribute
   var status = $(this)
-  .closest(".list-group")
-  .attr("id")
-  .replace("list-", "");
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
 
   // get the task's position in the list of other li elements
   var index = $(this)
-  .closest(".list-group-item")
-  .index();
+   .closest(".list-group-item")
+   .index();
 
   tasks[status][index].text = text;
   saveTasks();
 
   // recreate p element
   var taskP = $("<p>")
-  .addClass("m-1")
-  .text(text);
+    .addClass("m-1")
+    .text(text);
 
   // replace the textarea with the p element 
   $(this).replaceWith(taskP);
@@ -95,14 +98,14 @@ $(".list-group").on("click", "span", function() {
 
   // get current text
   var date = $(this)
-  .text()
-  .trim();
+    .text()
+   .trim();
 
   //create a new input element
   var dateInput = $("<input>")
-  .attr("type", "text")
-  .addClass("form-control")
-  .val(date);
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
 
   //swap out elements
   $(this).replaceWith(dateInput);
@@ -115,19 +118,19 @@ $(".list-group").on("click", "span", function() {
 $(".list-item-group").on("blur", "input[type='text']", function() {
   // get current date 
   var date = $(this)
-  .val()
-  .trim();
+    .val()
+    .trim();
 
   // get the parent ul's id attribute
   var status = $(this)
-  .closest(".list-group")
-  .attr("id")
-  .replace("list-", "");
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
 
   //get the tasks position in the list of other li elements
   var index = $(this)
-  .closest(".list-group-item")
-  .index();
+    .closest(".list-group-item")
+    .index();
 
   // update task in array and re-save to localStorage
   task[status][index].date = date;
@@ -135,11 +138,66 @@ $(".list-item-group").on("blur", "input[type='text']", function() {
 
   // recreate span element with bootstrap classes
   var taskSpan = $("<span>")
-  .addClass("badge badge-primary badge-pill")
-  .text(date);
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+});
+
+
+// turning the ul elements sortable using jQueryUI
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr=[];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text=$(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date=$(this)
+        .find("span")
+        .text()
+        .trim();
+
+    // add task data to the temp array as an object
+    tempArr.push({
+      text: text,
+      date: date
+    });
+  });
+
+  // trim down list's ID to match object property
+  var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+  // update array on tasks object and save
+  tasks[arrName] = tempArr;
+  saveTasks();
+
+  console.log(tempArr);
+  }
 });
 
 // modal was triggered
