eaf3da50318b194ed9a937f159068ebd39ddba3a
JAMart <jorge.martinez1597@gmail.com>
2021-06-17 11:45:27 -0400

    added drag and drop functionality on mobile and desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 var tasks = {};
 
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function (event, ui) {
+    console.log("out");
+  }
+});
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
